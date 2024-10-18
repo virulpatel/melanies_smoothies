9998cd4d5f9d0bbb374543a4065c5e1330e6a420
virulpatel <virulpatel@gmail.com>
2024-10-18 17:57:07 -0400

    Update streamline_app.py
    Adding API Request

--- streamline_app.py
+++ streamline_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -44,3 +45,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
